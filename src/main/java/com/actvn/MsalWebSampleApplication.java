ffd799e465efa8d99275110be403b6a37875474e
tuan.tn1 <ngoctuan.ktmm@gmail.com>
2021-05-09 19:18:54 +0700

    first commit

---
+++ src/main/java/com/actvn/MsalWebSampleApplication.java
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.actvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsalWebSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsalWebSampleApplication.class, args);
+    }
+}
