aa58fabb6adc7347e0dd3f37f5b57e14fd6cf019
tuan.tn1 <ngoctuan.ktmm@gmail.com>
2021-05-10 11:17:22 +0700

    Add log to debug

--- src/main/java/com/actvn/AuthPageController.java
+++ src/main/java/com/actvn/AuthPageController.java
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 package com.actvn;
 
 import java.io.IOException;
@@ -29,111 +28,115 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AuthPageController {
 
-	@Autowired
-	AuthHelper authHelper;
+    @Autowired
+    AuthHelper authHelper;
 
-	@RequestMapping("/msal4jsample")
-	public String homepage() {
-		return "index";
-	}
+    @RequestMapping("/msal4jsample")
+    public String homepage() {
+        System.out.println("homepage");
+        return "index";
+    }
 
-	@RequestMapping("/msal4jsample/secure/aad")
-	public ModelAndView securePage(HttpServletRequest httpRequest) throws ParseException {
-		ModelAndView mav = new ModelAndView("auth_page");
-		System.out.println("mav=" + mav);
-		setAccountInfo(mav, httpRequest);
+    @RequestMapping("/msal4jsample/secure/aad")
+    public ModelAndView securePage(HttpServletRequest httpRequest) throws ParseException {
+        System.out.println("securePage");
 
-		return mav;
-	}
+        ModelAndView mav = new ModelAndView("auth_page");
+        System.out.println("mav=" + mav);
+        setAccountInfo(mav, httpRequest);
 
-	@RequestMapping("/msal4jsample/sign_out")
-	public void signOut(HttpServletRequest httpRequest, HttpServletResponse response) throws IOException {
+        return mav;
+    }
 
-		httpRequest.getSession().invalidate();
+    @RequestMapping("/msal4jsample/sign_out")
+    public void signOut(HttpServletRequest httpRequest, HttpServletResponse response) throws IOException {
+        System.out.println("signOut");
+        httpRequest.getSession().invalidate();
 
-		String endSessionEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/logout";
+        String endSessionEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/logout";
 
-		String redirectUrl = "https://sso-with-azuread.herokuapp.com/msal4jsample/";
-		response.sendRedirect(
-				endSessionEndpoint + "?post_logout_redirect_uri=" + URLEncoder.encode(redirectUrl, "UTF-8"));
-	}
+        String redirectUrl = "https://sso-with-azuread.herokuapp.com/msal4jsample/";
+        response.sendRedirect(
+                endSessionEndpoint + "?post_logout_redirect_uri=" + URLEncoder.encode(redirectUrl, "UTF-8"));
+    }
 
-	@RequestMapping("/msal4jsample/graph/me")
-	public ModelAndView getUserFromGraph(HttpServletRequest httpRequest, HttpServletResponse httpResponse)
-			throws Throwable {
+    @RequestMapping("/msal4jsample/graph/me")
+    public ModelAndView getUserFromGraph(HttpServletRequest httpRequest, HttpServletResponse httpResponse)
+            throws Throwable {
+        System.out.println("getUserFromGraph");
 
-		IAuthenticationResult result;
-		ModelAndView mav;
-		try {
-			result = authHelper.getAuthResultBySilentFlow(httpRequest, httpResponse);
-		} catch (ExecutionException e) {
-			if (e.getCause() instanceof MsalInteractionRequiredException) {
+        IAuthenticationResult result;
+        ModelAndView mav;
+        try {
+            result = authHelper.getAuthResultBySilentFlow(httpRequest, httpResponse);
+        } catch (ExecutionException e) {
+            if (e.getCause() instanceof MsalInteractionRequiredException) {
 
-				// If silent call returns MsalInteractionRequired, then redirect to
-				// Authorization endpoint
-				// so user can consent to new scopes
-				String state = UUID.randomUUID().toString();
-				String nonce = UUID.randomUUID().toString();
+                // If silent call returns MsalInteractionRequired, then redirect to
+                // Authorization endpoint
+                // so user can consent to new scopes
+                String state = UUID.randomUUID().toString();
+                String nonce = UUID.randomUUID().toString();
 
-				SessionManagementHelper.storeStateAndNonceInSession(httpRequest.getSession(), state, nonce);
-				String authorizationCodeUrl = authHelper.getAuthorizationCodeUrl(httpRequest.getParameter("claims"),
-						"User.Read", authHelper.getRedirectUriGraph(), state, nonce);
+                SessionManagementHelper.storeStateAndNonceInSession(httpRequest.getSession(), state, nonce);
+                String authorizationCodeUrl = authHelper.getAuthorizationCodeUrl(httpRequest.getParameter("claims"),
+                        "User.Read", authHelper.getRedirectUriGraph(), state, nonce);
 
-				return new ModelAndView("redirect:" + authorizationCodeUrl);
-			} else {
+                return new ModelAndView("redirect:" + authorizationCodeUrl);
+            } else {
 
-				mav = new ModelAndView("error");
-				mav.addObject("error", e);
-				return mav;
-			}
-		}
+                mav = new ModelAndView("error");
+                mav.addObject("error", e);
+                return mav;
+            }
+        }
 
-		if (result == null) {
-			mav = new ModelAndView("error");
-			mav.addObject("error", new Exception("AuthenticationResult not found in session."));
-		} else {
-			mav = new ModelAndView("auth_page");
-			setAccountInfo(mav, httpRequest);
+        if (result == null) {
+            mav = new ModelAndView("error");
+            mav.addObject("error", new Exception("AuthenticationResult not found in session."));
+        } else {
+            mav = new ModelAndView("auth_page");
+            setAccountInfo(mav, httpRequest);
 
-			try {
-				mav.addObject("userInfo", getUserInfoFromGraph(result.accessToken()));
+            try {
+                mav.addObject("userInfo", getUserInfoFromGraph(result.accessToken()));
 
-				return mav;
-			} catch (Exception e) {
-				mav = new ModelAndView("error");
-				mav.addObject("error", e);
-			}
-		}
-		return mav;
-	}
+                return mav;
+            } catch (Exception e) {
+                mav = new ModelAndView("error");
+                mav.addObject("error", e);
+            }
+        }
+        return mav;
+    }
 
-	private String getUserInfoFromGraph(String accessToken) throws Exception {
-		// Microsoft Graph user endpoint
-		URL url = new URL(authHelper.getMsGraphEndpointHost() + "v1.0/me");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    private String getUserInfoFromGraph(String accessToken) throws Exception {
+        // Microsoft Graph user endpoint
+        URL url = new URL(authHelper.getMsGraphEndpointHost() + "v1.0/me");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-		// Set the appropriate header fields in the request header.
-		conn.setRequestProperty("Authorization", "Bearer " + accessToken);
-		conn.setRequestProperty("Accept", "application/json");
+        // Set the appropriate header fields in the request header.
+        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+        conn.setRequestProperty("Accept", "application/json");
 
-		String response = HttpClientHelper.getResponseStringFromConn(conn);
+        String response = HttpClientHelper.getResponseStringFromConn(conn);
 
-		int responseCode = conn.getResponseCode();
-		if (responseCode != HttpURLConnection.HTTP_OK) {
-			throw new IOException(response);
-		}
+        int responseCode = conn.getResponseCode();
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            throw new IOException(response);
+        }
 
-		JSONObject responseObject = HttpClientHelper.processResponse(responseCode, response);
-		return responseObject.toString();
-	}
+        JSONObject responseObject = HttpClientHelper.processResponse(responseCode, response);
+        return responseObject.toString();
+    }
 
-	private void setAccountInfo(ModelAndView model, HttpServletRequest httpRequest) throws ParseException {
-		IAuthenticationResult auth = SessionManagementHelper.getAuthSessionObject(httpRequest);
-		System.out.println("auth=" + auth);
+    private void setAccountInfo(ModelAndView model, HttpServletRequest httpRequest) throws ParseException {
+        IAuthenticationResult auth = SessionManagementHelper.getAuthSessionObject(httpRequest);
+        System.out.println("auth=" + auth);
 
-		String tenantId = JWTParser.parse(auth.idToken()).getJWTClaimsSet().getStringClaim("tid");
+        String tenantId = JWTParser.parse(auth.idToken()).getJWTClaimsSet().getStringClaim("tid");
 
-		model.addObject("tenantId", tenantId);
-		model.addObject("account", SessionManagementHelper.getAuthSessionObject(httpRequest).account());
-	}
+        model.addObject("tenantId", tenantId);
+        model.addObject("account", SessionManagementHelper.getAuthSessionObject(httpRequest).account());
+    }
 }
